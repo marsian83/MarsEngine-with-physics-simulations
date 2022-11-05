0f65e60df17bea9ea4c5289a748a8c2218a9043a
marsian8 <spanarve@gmail.com>
2022-11-05 13:19:06 +0530

    changes

--- Engine/build-custom.bat
+++ Engine/build-custom.bat
@@ -1,2 +1,2 @@
 @echo off
-g++ -Isrc/include -Lsrc/lib custom.cpp src/*.cpp -o out -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -w && del custom.cpp && out
+g++ -Isrc/include -Lsrc/lib custom.cpp src/*.cpp -o out -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -w && del custom.cpp && out
